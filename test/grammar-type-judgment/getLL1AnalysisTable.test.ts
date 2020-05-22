b6a5b9b8d6339b0dc6d1135875e50c471d8e7aaa
crawler995 <zhang_995@foxmail.com>
2020-05-22 22:07:23 +0800

    add two tests for getLL1AnalysisTable() (all passed!)

--- test/grammar-type-judgment/getLL1AnalysisTable.test.ts
+++ test/grammar-type-judgment/getLL1AnalysisTable.test.ts
@@ -1,7 +1,7 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import { Grammar, EMPTY, Production } from '../../src/grammar-type-judgment/types/grammar';
+import { Grammar, EMPTY, Production, END } from '../../src/grammar-type-judgment/types/grammar';
 import getLL1AnalysisTable from '../../src/grammar-type-judgment/getLL1AnalysisTable';
 
 describe('getLL1AnalysisTable.ts (getLL1AnalysisTable())', () => {
@@ -69,13 +69,159 @@ describe('getLL1AnalysisTable.ts (getLL1AnalysisTable())', () => {
       };
       expect(getLL1AnalysisTable(grammar)).to.deep.equal({
         rows: ['E', "E'", 'T', "T'", 'F'],
-        columns: ['+', '*', '(', ')', 'i', '#'],
+        columns: ['+', '*', '(', ')', 'i', END],
         relationships: [
           [null, null, production1, null, production1, null],
           [production2, null, null, production3, null, production3],
           [null, null, production4, null, production4, null],
           [production5, production6, null, production5, null, production5],
           [null, null, production7, null, production8, null]
+        ]
+      });
+    });
+  });
+
+  describe('S->Ap|Bq, A->a|cA, B->b|dB', () => {
+    const grammar: Grammar = {
+      nonTerminals: ['S', 'A', 'B'],
+      terminals: ['a', 'b', 'c', 'd', 'p', 'q'],
+      productions: [
+        {
+          left: ['S'],
+          right: [
+            ['A', 'p'],
+            ['B', 'q']
+          ]
+        },
+        {
+          left: ['A'],
+          right: [['a'], ['c', 'A']]
+        },
+        {
+          left: ['B'],
+          right: [['b'], ['d', 'B']]
+        }
+      ],
+      startSymbol: 'S'
+    };
+
+    it('LL(1) analysis table should be ...', () => {
+      const production1: Production = {
+        left: ['S'],
+        right: [['A', 'p']]
+      };
+      const production2: Production = {
+        left: ['S'],
+        right: [['B', 'q']]
+      };
+      const production3: Production = {
+        left: ['A'],
+        right: [['a']]
+      };
+      const production4: Production = {
+        left: ['A'],
+        right: [['c', 'A']]
+      };
+      const production5: Production = {
+        left: ['B'],
+        right: [['b']]
+      };
+      const production6: Production = {
+        left: ['B'],
+        right: [['d', 'B']]
+      };
+
+      expect(getLL1AnalysisTable(grammar)).to.deep.equal({
+        rows: ['S', 'A', 'B'],
+        columns: ['a', 'b', 'c', 'd', 'p', 'q', END],
+        relationships: [
+          [production1, production2, production1, production2, null, null, null],
+          [production3, null, production4, null, null, null, null],
+          [null, production5, null, production6, null, null, null]
+        ]
+      });
+    });
+  });
+
+  describe('S->(X|E]|F), X->E)|F], E->A, F->A, A->EMPTY', () => {
+    const grammar: Grammar = {
+      nonTerminals: ['S', 'X', 'E', 'F', 'A'],
+      terminals: ['(', ']', ')'],
+      productions: [
+        {
+          left: ['S'],
+          right: [
+            ['(', 'X'],
+            ['E', ']'],
+            ['F', ')']
+          ]
+        },
+        {
+          left: ['X'],
+          right: [
+            ['E', ')'],
+            ['F', ']']
+          ]
+        },
+        {
+          left: ['E'],
+          right: [['A']]
+        },
+        {
+          left: ['F'],
+          right: [['A']]
+        },
+        {
+          left: ['A'],
+          right: [[EMPTY]]
+        }
+      ],
+      startSymbol: 'S'
+    };
+
+    it('LL(1) analysis table should be ...', () => {
+      const production1: Production = {
+        left: ['S'],
+        right: [['(', 'X']]
+      };
+      const production2: Production = {
+        left: ['S'],
+        right: [['E', ']']]
+      };
+      const production3: Production = {
+        left: ['S'],
+        right: [['F', ')']]
+      };
+      const production4: Production = {
+        left: ['X'],
+        right: [['F', ']']]
+      };
+      const production5: Production = {
+        left: ['X'],
+        right: [['E', ')']]
+      };
+      const production6: Production = {
+        left: ['E'],
+        right: [['A']]
+      };
+      const production7: Production = {
+        left: ['F'],
+        right: [['A']]
+      };
+      const production8: Production = {
+        left: ['A'],
+        right: [[EMPTY]]
+      };
+
+      expect(getLL1AnalysisTable(grammar)).to.deep.equal({
+        rows: ['S', 'X', 'E', 'F', 'A'],
+        columns: ['(', ']', ')', END],
+        relationships: [
+          [production1, production2, production3, null],
+          [null, production4, production5, null],
+          [null, production6, production6, null],
+          [null, production7, production7, null],
+          [null, production8, production8, null]
         ]
       });
     });
